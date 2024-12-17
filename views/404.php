5c80f5dd7159dc3b3f0632c859838fd9a64259bb
VolaSoft <volasoft@yahoo.com>
2024-12-18 06:02:07 +0800

    Save 2024-12-18

--- views/404.php
+++ views/404.php
@@ -3,16 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title><?php echo $appName ?> | 404</title>
+    <style>
+        .center{
+            text-align:center;
+        }
+    </style>
 </head>
-<body>
-    <nav>
-        <menu>
-            <a href="/">Home</a>
-            <a href="/about.php">About</a>
-            <a href="/contact.php">Contact</a>
-        </menu>
-    </nav>
-    <h1>Ooops! Seems like the page you are trying to reach does not exist.</h1>
+<body class="center">
+    <header>
+        <?php require_once "menu.php"; ?>
+    </header>
+
+    <h1>Ooops! </h1>
+    <h4> Seems like the page you are trying to reach does not exist.</h4>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
